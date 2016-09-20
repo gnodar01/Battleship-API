5cf2a6e4e53633c6b7ccbc9c41a50bccca5cba8a
gnodar01 <gnodar01@gmail.com>
2016-09-20 08:15:19 -0400

    Complete functionality for sending reminder emails cron job

--- main.py
+++ main.py
@@ -9,14 +9,34 @@ from google.appengine.api import mail, app_identity
 from google.appengine.ext import ndb
 from api import BattleshipAPI
 from models.ndbModels import Game, User
+from utils import get_by_urlsafe
 
 
 class SendReminderEmail(webapp2.RequestHandler):
     def get(self):
-        """Send Users with unfinished games reminders, once a day"""
+        """Send Users with unfinished games reminders, once a day, if it's their turn to play"""
         app_id = app_identity.get_application_id()
         unfinished_games = Game.query(ndb.AND(Game.game_started == True, Game.game_over == False)).fetch()
-        print unfinished_games
+        users = {}
+        for game in unfinished_games:
+            user_id = game.player_turn.id()
+            user = User.get_by_id(user_id)
+            if user_id in users:
+                users[user_id]['num_games_in_progress'] += 1
+            else:
+                users[user_id] = {'num_games_in_progress': 1,
+                                  'email': user.email,
+                                  'name': user.name}
+        for u_id in users:
+            subject = 'This is a reminder!'
+            body = 'Hello {}, you currently have {} Battle Ship games in progress!'.format(users[u_id]['name'], users[u_id]['num_games_in_progress'])
+            # The arguments to send_mail are:
+            # from, to, subject, body
+            mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
+                           users[u_id]['email'],
+                           subject,
+                           body)
+        
 
 
 class SomethingElse(webapp2.RequestHandler):
