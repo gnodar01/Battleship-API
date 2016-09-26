74afacbcfffcb669e89fa4dcee456a3693dff233
gnodar01 <gnodar01@gmail.com>
2016-09-26 00:33:22 -0400

    Rename protorpcModels to repsonses

--- models/responses.py
+++ models/responses.py
@@ -1,7 +1,3 @@
-"""models.py - This file contains the class definitions for the Datastore
-entities used by the Game. Because these classes are also regular Python
-classes they can include methods (such as 'to_form' and 'new_game')."""
-
 import random
 from datetime import date
 from protorpc import messages, message_types
@@ -17,7 +13,8 @@ class UserForm(messages.Message):
     email = messages.StringField(2, required=True)
 
 class GameStatusMessage(messages.Message):
-    """GameStatus-- outbound message describing a given game's current status"""
+    """GameStatus-- outbound message describing
+    a given game's current status"""
     player_one = messages.StringField(1, required=True)
     player_two = messages.StringField(2, required=True)
     player_one_pieces_loaded = messages.StringField(3, required=True)
@@ -66,3 +63,4 @@ class MoveDetails(messages.Message):
 class GameHistory(messages.Message):
     """History of all moves played for a given game"""
     moves = messages.MessageField(MoveDetails, 1, repeated=True)
+
