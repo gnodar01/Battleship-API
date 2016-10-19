89eb31680500762c63608ac02989584584b7dedb
Nodar Gogoberidze <gnodar01@gmail.com>
2016-10-19 02:40:57 -0400

    Add players unique, game open, and registered player validation

--- validators.py
+++ validators.py
@@ -28,3 +28,20 @@ def check_email_exists(email):
     if User.query(User.email == email).get():
         raise endpoints.ConflictException(
             'A User with that E-Mail already exists')
+
+
+def check_player_registered(game, player):
+    if game.player_one != player.key and game.player_two != player.key:
+        raise endpoints.ConflictException(
+            '{} is not registered for this game'.format(player.name))
+
+
+def check_players_unique(player_one_name, player_two_name):
+    if player_one_name == player_two_name:
+        raise endpoints.ConflictException(
+            'Player one cannot be the same as player two.')
+
+
+def check_game_open(game):
+    if game.player_two:
+        raise endpoints.ConflictException('This game is already full!')
