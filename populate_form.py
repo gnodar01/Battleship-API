12c29fcb15595fa71879a32cb608d16c5518ea39
Nodar Gogoberidze <gnodar01@gmail.com>
2016-10-19 02:40:17 -0400

    Add copy_game_to_form

--- populate_form.py
+++ populate_form.py
@@ -1,10 +1,35 @@
 from models.responses import (
-    UserForm
+    UserForm,
+    GameStatusMessage
 )
 
 
-def _copy_user_to_form(self, user_obj):
+def _copy_user_to_form(user_obj):
     user_form = UserForm()
     setattr(user_form, 'name', getattr(user_obj, 'name'))
     setattr(user_form, 'email', getattr(user_obj, 'email'))
     return user_form
+
+
+def copy_game_to_form(game_obj):
+    game_form = GameStatusMessage()
+
+    setattr(game_form, "game_key", str(game_obj.key.urlsafe()))
+
+    for field in game_form.all_fields():
+        if (field.name == "player_one" or field.name == "player_two" or
+                field.name == "player_turn" or field.name == "winner"):
+
+            player_key = getattr(game_obj, field.name)
+
+            if player_key:
+                player = player_key.get()
+                setattr(game_form, field.name, player.name)
+            else:
+                setattr(game_form, field.name, "None")
+
+        elif hasattr(game_obj, field.name):
+            setattr(game_form, field.name,
+                    str(getattr(game_obj, field.name)))
+
+    return game_form
