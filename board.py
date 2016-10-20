4413343e2b21ec51752e652843805bdac7ba054b
Nodar Gogoberidze <gnodar01@gmail.com>
2016-10-20 08:15:20 -0400

    Add documentation on limits of extending ROWS and COLUMNS

--- board.py
+++ board.py
@@ -6,6 +6,15 @@ PIECES = {
     'patrol_ship': {'name': 'Patrol Ship', 'spaces': 2}
 }
 
+"""COLUMNS can be extended, but should never contain a digit.
+if .isdigit() returns true on any element within COLUMNS,
+the code will break."""
 COLUMNS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+"""ROWS can be extended, but should only contain strings that
+can be converted into digits. if .isdigit() returns false on
+any elemnt in ROWS, the code will break.
+Should also be ascending order, starting with 1,
+and incrementing by 1."""
 ROWS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+# len(COLUMNS) == len(ROWS) should always return True.
 GRID = [(column + row) for column in COLUMNS for row in ROWS]
